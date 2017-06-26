51c6ad522c4024450f93f3bfa1744eba4e9b34c9
Kirsten Frager <kirstenfrager@Kirstens-MacBook-Air.local>
2017-06-26 15:21:54 +1000

    decorators

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+// decorator provides a bit of additional data about the component
 @Component({
+  // names the element injected into the DOM
   selector: 'page-home',
+  // links to the template file
   templateUrl: 'home.html'
 })
 export class HomePage {
